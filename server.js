f310108e30c63447e8e9ed5513f6f5707e677d80
Jonghyeok <jhpmuscle@gmail.com>
2023-05-26 17:21:44 +0900

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mykey = require('./mykey.js');
 app.use(bodyParser.urlencoded({ extended: true }));
+const MongoClient = require('mongodb').MongoClient;
 
-app.listen(8080, function () {
-    console.log('listening on 8080');
-});
+
+MongoClient.connect(`mongodb+srv://admin:${mykey.dbkey}@cluster0.oawj1is.mongodb.net/?retryWrites=true&w=majority`, function (error, client) {
+    if (error) return console.log(error);
+
+    app.listen(8080, function () {
+        console.log('listening on 8080')
+    })
+})
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/pages/index.html');
