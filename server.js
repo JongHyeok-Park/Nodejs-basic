d2a5264b155ca22e5c86f2ae1522e8c0debc9e7e
Jonghyeok <jhpmuscle@gmail.com>
2023-06-05 20:59:26 +0900

    edit page

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mykey = require('./mykey.js');
 const { list } = require('mongodb/lib/gridfs/grid_store.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require('mongodb').MongoClient;
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
@@ -63,6 +65,13 @@ app.get('/detail/:id', function (req, res) {
     db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
         console.log(result);
         res.render('detail.ejs', { data: result });
+    })
+
+})
+
+app.get('/edit/:id', function (req, res) {
+    db.collection('post').findOne({ _id: parseInt(req.params.id) }, function (error, result) {
+        res.render('edit.ejs', { post: result });
     })
 
 })
